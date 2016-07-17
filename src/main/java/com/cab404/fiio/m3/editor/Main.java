ae672cffd925b44c7abe37d1559237ab77411012
cab404 <me@cab404.ru>
2016-07-17 09:52:47 +0300

    support for file as argument

--- src/main/java/com/cab404/fiio/m3/editor/Main.java
+++ src/main/java/com/cab404/fiio/m3/editor/Main.java
@@ -1,12 +1,16 @@
 package com.cab404.fiio.m3.editor;
 
+import java.io.File;
+
 /**
  * @author cab404
  */
 public class Main {
 
     public static void main(String[] args) {
-        new MainScreen().show();
+        MainScreen screen = new MainScreen();
+        screen.show();
+        if (args.length > 0) screen.onFileSelected(new File(args[0]));
     }
 
 }
